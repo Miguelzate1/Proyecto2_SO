feb8826067f898c89f0a1d1d426ce4a7d6420e2d
Miguelzate1 <alzatem114@gmail.com>
2026-09-23 09:02:34 -0500

    Guardar configuracion de herencia de trace en proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,7 +124,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
-  p->trace_syscall = 0;
+  p->trace_syscall = 0; 
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0) {
     freeproc(p);
@@ -275,7 +275,7 @@ kfork(void)
     return -1;
   }
   np->sz = p->sz;
-
+  np->trace_syscall = p->trace_syscall;
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 

e3f47b7b24a52c9d2b7c11d0b160cb9940ab9e9c
squintere <samuelqe3@gmail.com>
2026-09-23 22:20:29 -0500

    Implementa countrunnable en proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -699,4 +699,22 @@ procdump(void)
     printk("%d %s %s", p->pid, state, p->name);
     printk("\n");
   }
+
+  // Recorre la tabla de procesos y cuenta cuántos están en estado RUNNABLE.
+  int
+  countrunnable(void)
+  {
+    struct proc *p;
+    int n = 0;
+
+    for(p = proc; p < &proc[NPROC]; p++){
+      acquire(&p->lock);
+      if(p->state == RUNNABLE)
+        n++;
+      release(&p->lock);
+    }
+
+    return n;
+  }
 }
+
